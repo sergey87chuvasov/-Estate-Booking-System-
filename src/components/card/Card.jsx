d0239f26fd0ef0c277035736027881286ce0cd78
Sergey87 <serge.techno@gmail.com>
2024-03-16 18:02:01 +0300

    added List Page section - 3 part

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,7 +1,44 @@
 import './card.scss';
+import { Link } from 'react-router-dom';
 
-function Card() {
-  return <div className='card'>Card</div>;
+function Card({ item }) {
+  return (
+    <div className='card'>
+      <Link to={`/${item.id}`} className='imageContainer'>
+        <img src={item.img} alt='img' />
+      </Link>
+      <div className='textContainer'>
+        <h2 className='title'>
+          <Link to={`/${item.id}`}>{item.title}</Link>
+        </h2>
+        <p className='address'>
+          <img src='/pin.png' alt='pin' />
+          <span>{item.address}</span>
+        </p>
+        <p className='price'>$ {item.price}</p>
+        <div className='bottom'>
+          <div className='features'>
+            <div className='feature'>
+              <img src='/bed.png' alt='feature' />
+              <span>{item.bedroom} bedroom</span>
+            </div>
+            <div className='feature'>
+              <img src='/bath.png' alt='feature' />
+              <span>{item.bathroom} bathroom</span>
+            </div>
+          </div>
+          <div className='icons'>
+            <div className='icon'>
+              <img src='/save.png' alt='icon' />
+            </div>
+            <div className='icon'>
+              <img src='/chat.png' alt='icon' />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
