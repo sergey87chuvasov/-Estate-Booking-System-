a76521570c86c0361ac301c2a64908a7e1662dd5
Sergey87 <serge.techno@gmail.com>
2024-03-17 20:09:38 +0300

    added Leaflet Map in project

---
+++ src/components/map/Map.jsx
@@ -0,0 +1,26 @@
+import './map.scss';
+import 'leaflet/dist/leaflet.css';
+import Pin from '../pin/Pin';
+import { MapContainer, TileLayer } from 'react-leaflet';
+
+function Map({ items }) {
+  return (
+    <MapContainer
+      className='map'
+      center={[52.4797, -1.90269]}
+      zoom={7}
+      scrollWheelZoom={false}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+      />
+
+      {items.map((item) => {
+        <Pin item={item} key={item.id} />;
+      })}
+    </MapContainer>
+  );
+}
+
+export default Map;
