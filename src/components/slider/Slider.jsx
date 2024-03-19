4c09132fc762375986c0bfc09be4e13b72d39202
Sergey87 <serge.techno@gmail.com>
2024-03-19 22:27:58 +0300

    added slider - 1 part

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,7 +1,30 @@
 import './slider.scss';
 
-function Slider() {
-  return <div className='slider'>Slider</div>;
+function Slider({ images }) {
+  return (
+    <div className='slider'>
+      <div className='fullSlider'>
+        <div className='arrow'>
+          <img src='/arrow.png' alt='arrow' />
+        </div>
+        <div className='imgContainer'>
+          <img src={images[0]} alt='img' />
+        </div>
+        <div className='arrow'>
+          <img src='/arrow.png' alt='arrow' className='right' />
+        </div>
+        <div className='close'>X</div>
+      </div>
+      <div className='bigImage'>
+        <img src={images[0]} alt='img' />
+      </div>
+      <div className='smallImages'>
+        {images.slice(1).map((image, index) => (
+          <img src={image} alt='img' key={index} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Slider;
