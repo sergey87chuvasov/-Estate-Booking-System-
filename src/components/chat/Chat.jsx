6f15de3b897f095809cff3d7dce75981f8be5897
Sergey87 <serge.techno@gmail.com>
2024-03-24 22:33:34 +0300

    added Chat Window Design

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,6 +1,8 @@
+import { useState } from 'react';
 import './chat.scss';
 
 function Chat() {
+  const [chat, setChat] = useState(true);
   return (
     <div className='chat'>
       <div className='messages'>
@@ -62,7 +64,60 @@ function Chat() {
           <p>Lorem ipsum dolor sit, amet elit...</p>
         </div>
       </div>
-      <div className='chatBox'>box</div>
+      {chat && (
+        <div className='chatBox'>
+          <div className='top'>
+            <div className='user'>
+              <img
+                src='https://images.pexels.com/photos/1374510/pexels-photo-1374510.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                alt='user'
+              />
+              Jhon Doe
+            </div>
+            <span className='close' onClick={() => setChat(null)}>
+              X
+            </span>
+          </div>
+          <div className='center'>
+            <div className='chatMessage'>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className='chatMessage own'>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className='chatMessage'>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className='chatMessage own'>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className='chatMessage'>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className='chatMessage own'>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className='chatMessage'>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className='chatMessage own'>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <span>1 hour ago</span>
+            </div>
+          </div>
+          <div className='bottom'>
+            <textarea></textarea>
+            <button>Send</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
