7be1f8396e815e53e4dc71407fd3cf854be19721
Sergey87 <serge.techno@gmail.com>
2024-04-04 23:35:53 +0300

    added Express.js Auth Tutorial

--- api/app.js
+++ api/app.js
@@ -1,37 +1,13 @@
 import express from 'express';
+import authRoute from './routes/auth.route.js';
 import postRoute from './routes/post.route.js';
 
 const app = express();
 
+app.use(express.json());
+
 app.use('/api/posts', postRoute);
-
-// app.use('/api/test', (req, res) => {
-//   res.send('it works');
-// });
-
-// app.use('/api/auth/register', (req, res) => {
-//   res.send('it works');
-// });
-
-// app.use('/api/auth/login', (req, res) => {
-//   res.send('it works');
-// });
-
-// app.use('/api/auth/logout', (req, res) => {
-//   res.send('it works');
-// });
-
-// app.use('/api/posts/', (req, res) => {
-//   res.send('it works');
-// });
-
-// app.use('/api/posts/', (req, res) => {
-//   res.send('it works');
-// });
-
-// app.use('/api/posts/1111', (req, res) => {
-//   res.send('it works');
-// });
+app.use('/api/auth', authRoute);
 
 app.listen(8800, () => {
   console.log('Server is running');
