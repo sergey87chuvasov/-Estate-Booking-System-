83a96e66276743a6e16f4f82fb843935aa6af13e
Sergey87 <serge.techno@gmail.com>
2024-04-04 22:44:25 +0300

    added Understanding Express.js Routes

--- api/app.js
+++ api/app.js
@@ -1,10 +1,37 @@
 import express from 'express';
+import postRoute from './routes/post.route.js';
 
 const app = express();
 
-app.use('/api/test', (req, res) => {
-  res.send('it works');
-});
+app.use('/api/posts', postRoute);
+
+// app.use('/api/test', (req, res) => {
+//   res.send('it works');
+// });
+
+// app.use('/api/auth/register', (req, res) => {
+//   res.send('it works');
+// });
+
+// app.use('/api/auth/login', (req, res) => {
+//   res.send('it works');
+// });
+
+// app.use('/api/auth/logout', (req, res) => {
+//   res.send('it works');
+// });
+
+// app.use('/api/posts/', (req, res) => {
+//   res.send('it works');
+// });
+
+// app.use('/api/posts/', (req, res) => {
+//   res.send('it works');
+// });
+
+// app.use('/api/posts/1111', (req, res) => {
+//   res.send('it works');
+// });
 
 app.listen(8800, () => {
   console.log('Server is running');
