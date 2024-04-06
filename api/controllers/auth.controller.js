c1cf41b0a544a2e7a9fb9ae0277276538396cb54
Sergey87 <serge.techno@gmail.com>
2024-04-06 21:13:20 +0300

    added work with prisma and mongodb

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcrypt from 'bcrypt';
+import prisma from '../lib/prisma.js';
 
 export const register = async (req, res) => {
   const { username, email, password } = req.body;
@@ -6,6 +7,17 @@ export const register = async (req, res) => {
   const hashedPassword = await bcrypt.hash(password, 10);
 
   console.log(hashedPassword);
+
+  const newUser = await prisma.user.create({
+    data: {
+      username,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  console.log(newUser);
+  res.status(201).json({ message: 'User created successfully' });
 };
 
 export const login = (req, res) => {};
