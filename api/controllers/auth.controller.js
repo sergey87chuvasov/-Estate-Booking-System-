7b7f128ac3eb89e5df67cdb7f3965145df99feda
Sergey87 <serge.techno@gmail.com>
2024-06-07 01:31:09 +0300

    fixed project

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import prisma from '../lib/prisma.js';
+import process from 'process';
 
 export const register = async (req, res) => {
   const { username, email, password } = req.body;
@@ -18,7 +19,7 @@ export const register = async (req, res) => {
       },
     });
 
-    // console.log(newUser);
+    console.log(newUser);
     // console.log(hashedPassword);
     // console.log(req.body);
 
@@ -48,16 +49,18 @@ export const login = async (req, res) => {
 
     // res.setHeader('Set-Cookie', 'test=', +'myValue').json('success');
 
+    const age = 1000 * 60 * 60 * 24 * 7;
+
     const token = jwt.sign(
       {
         id: user.id,
       },
-      ''
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: age }
     );
 
-    const age = 1000 * 60 * 60 * 24 * 7;
     res
-      .cookie('test2', 'myValue2', {
+      .cookie('token', token, {
         httpOnly: true,
         // secure: true,
         maxAge: age,
@@ -70,4 +73,6 @@ export const login = async (req, res) => {
   }
 };
 
-export const logout = (req, res) => {};
+export const logout = (req, res) => {
+  res.clearCookie('token').status(200).json({ message: 'Logout Successful' });
+};
