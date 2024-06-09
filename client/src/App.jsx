bbadc2fc61c5bc5c3bae11a27ad643e48beda22d
Sergey87 <serge.techno@gmail.com>
2024-06-09 20:01:24 +0300

    added login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import ListPage from './routes/listPage/listPage';
 import Layout from './routes/layout/layout';
 import SinglePage from './routes/singlePage/singlePage';
 import ProfilePage from './routes/profilePage/profilePage';
+import Login from './routes/login/login';
 import Register from './routes/register/register';
 
 function App() {
@@ -27,6 +28,10 @@ function App() {
         {
           path: '/profile',
           element: <ProfilePage />,
+        },
+        {
+          path: '/login',
+          element: <Login />,
         },
         {
           path: '/register',
