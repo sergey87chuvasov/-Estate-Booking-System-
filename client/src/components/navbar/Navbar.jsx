8dbc0ef1f8fba543366b2d0264bb64bc83967b96
Sergey87 <serge.techno@gmail.com>
2024-06-09 22:03:28 +0300

    work with JWT Cookie Auth - 2 part

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   const [open, setOpen] = useState(false);
 
-  const user = false;
+  const user = true;
   return (
     <nav>
       <div className='left'>
--- client/src/routes/login/login.jsx
+++ client/src/routes/login/login.jsx
@@ -1,20 +1,70 @@
-import "./login.scss";
-import { Link } from "react-router-dom";
+import { useState } from 'react';
+import './login.scss';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 function Login() {
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setIsLoading(true);
+
+    const formData = new FormData(e.target);
+
+    const username = formData.get('username');
+    const email = formData.get('email');
+    const password = formData.get('password');
+
+    // console.log(username, email, password);
+
+    try {
+      const res = await axios.post('http://localhost:8800/api/auth/register', {
+        username,
+        email,
+        password,
+      });
+
+      navigate('/login');
+
+      // console.log(res.data);
+    } catch (err) {
+      // console.log(err);
+      setError(err.response.data.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <div className="login">
-      <div className="formContainer">
-        <form>
+    <div className='login'>
+      <div className='formContainer'>
+        <form onSubmit={handleSubmit}>
           <h1>Welcome back</h1>
-          <input name="username" type="text" placeholder="Username" />
-          <input name="password" type="password" placeholder="Password" />
-          <button>Login</button>
-          <Link to="/register">{"Don't"} you have an account?</Link>
+          <input
+            name='username'
+            required
+            minLength={3}
+            maxLength={20}
+            type='text'
+            placeholder='Username'
+          />
+          <input
+            name='password'
+            required
+            type='password'
+            placeholder='Password'
+          />
+          <button disabled={isLoading}>Login</button>
+          {error && <span>{error}</span>}
+          <Link to='/register'>{"Don't"} you have an account?</Link>
         </form>
       </div>
-      <div className="imgContainer">
-        <img src="/bg.png" alt="" />
+      <div className='imgContainer'>
+        <img src='/bg.png' alt='' />
       </div>
     </div>
   );
