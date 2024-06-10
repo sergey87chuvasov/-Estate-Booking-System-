4b59852481f41d881f06c41f34494357bfd8c067
Sergey87 <serge.techno@gmail.com>
2024-06-10 23:44:36 +0300

    Protect Routes with JWT Cookie

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -54,6 +54,7 @@ export const login = async (req, res) => {
     const token = jwt.sign(
       {
         id: user.id,
+        isAdmin: true,
       },
       process.env.JWT_SECRET_KEY,
       { expiresIn: age }
---
+++ api/controllers/test.controller.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken';
+
+export const shouldBeLoggedIn = async (req, res) => {
+  const token = req.cookies.token;
+
+  if (!token) return res.status(401).json({ message: 'Not Auth' });
+
+  jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
+    if (err) return res.status(403).json({ message: 'Token is not Valid' });
+  });
+  res.status(200).json({ message: 'You are Auth' });
+};
+
+export const shouldBeAdmin = async (req, res) => {
+  const token = req.cookies.token;
+
+  if (!token) return res.status(401).json({ message: 'Not Auth' });
+
+  jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
+    if (err) return res.status(403).json({ message: 'Token is not Valid' });
+
+    if (!payload.isAdmin) {
+      return res.status(403).json({ message: 'Not Auth' });
+    }
+  });
+  res.status(200).json({ message: 'You are Auth' });
+};
