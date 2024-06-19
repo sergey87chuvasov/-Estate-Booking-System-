98413fb7e7f70db0bf58059fbc5210524656d6f0
Sergey87 <serge.techno@gmail.com>
2024-06-19 21:49:19 +0300

    added profile update page component

--- client/src/routes/profilePage/profilePage.jsx
+++ client/src/routes/profilePage/profilePage.jsx
@@ -5,6 +5,7 @@ import apiRequest from '../../lib/apiRequest';
 import { useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
 import { AuthContext } from '../../context/AuthContect';
+import { Link } from 'react-router-dom';
 
 function ProfilePage() {
   const { updateUser, currentUser } = useContext(AuthContext);
@@ -26,7 +27,9 @@ function ProfilePage() {
         <div className='wrapper'>
           <div className='title'>
             <h1>User Information</h1>
-            <button>Update Profile</button>
+            <Link to='/profile/update'>
+              <button>Update Profile</button>
+            </Link>
           </div>
           <div className='info'>
             <span>
