b06cef9160b3b132f3fb0e966b0db27162e5c475
Sergey87 <serge.techno@gmail.com>
2024-06-24 23:42:26 +0300

    Prisma MongoDB Post Modal - 2 part

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -2,9 +2,74 @@ import prisma from '../lib/prisma.js';
 
 export const getPosts = async (req, res) => {
   try {
-    res.status(200).json();
+    const posts = await prisma.post.findMany();
+    res.status(200).json(posts);
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: 'Fail to get posts' });
+  }
+};
+
+export const getPost = async (req, res) => {
+  const id = req.params;
+  try {
+    const post = await prisma.post.findUnique({
+      where: { id },
+    });
+    res.status(200).json(post);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Fail to get post' });
+  }
+};
+
+export const addPost = async (req, res) => {
+  const body = req.body;
+  const tokenUserId = req.userId;
+
+  try {
+    const newPost = await prisma.post.create({
+      data: {
+        ...body,
+        userId: tokenUserId,
+      },
+    });
+    res.status(200).json(newPost);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Fail to create post' });
+  }
+};
+
+export const updatePost = async (req, res) => {
+  try {
+    res.status(200).json();
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Fail to update posts' });
+  }
+};
+
+export const deletePost = async (req, res) => {
+  const id = req.params.id;
+  const tokenUserId = req.userId;
+
+  try {
+    const post = await prisma.post.findUnique({
+      where: { id },
+    });
+
+    if (post.userId !== tokenUserId) {
+      return res.status(403).json({ message: 'Not Auth!' });
+    }
+
+    await prisma.post.delete({
+      where: { id },
+    });
+
+    res.status(200).json({ message: 'Post del' });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Fail to delete posts' });
   }
 };
