306285a90f222516916064f815e29a641bb23a21
Sergey87 <serge.techno@gmail.com>
2024-07-03 17:49:12 +0300

    finished Prisma MongoDB Post Moda

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -11,10 +11,19 @@ export const getPosts = async (req, res) => {
 };
 
 export const getPost = async (req, res) => {
-  const id = req.params;
+  const id = req.params.id;
   try {
     const post = await prisma.post.findUnique({
       where: { id },
+      include: {
+        postDetail: true,
+        user: {
+          select: {
+            username: true,
+            avatar: true,
+          },
+        },
+      },
     });
     res.status(200).json(post);
   } catch (err) {
@@ -30,8 +39,11 @@ export const addPost = async (req, res) => {
   try {
     const newPost = await prisma.post.create({
       data: {
-        ...body,
+        ...body.postData,
         userId: tokenUserId,
+        postDetail: {
+          create: body.postDetail,
+        },
       },
     });
     res.status(200).json(newPost);
